a6bc4aaaa8ecba236d801b4c21cde78f7ca6de6c
MesosTables <carwbrown@gmail.com>
2016-02-04 23:53:55 -0500

    added in api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = '018bf44246c3cce7b87414648a2535f9';
-const CITY = "Raleigh";
-const URL = "http://api.openweathermap.org/data/2.5/forecast?q="
-+ CITY + ",us&appid=" + API_KEY;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+//will make the API request
+export function fetchWeather(city){
+	const url = `${ROOT_URL}&q=${city},us`;
+	//this will return a promise
+	const request = axios.get(url);
+
+	return{
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
+//bring in ActionCreator fetchWeather
+import {fetchWeather} from '../actions/index.js'
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 
-export default class SearchBar extends Component{
+export class SearchBar extends Component{
 	//set  up the components state
 	constructor(props){
 		super(props);
@@ -9,6 +13,8 @@ export default class SearchBar extends Component{
 //take the existing function onInputChange bind it to
 //this (of SearchBar) and set the function back to this.onInputChange
 		this.onInputChange = this.onInputChange.bind(this);
+		//this is a callback which needs to be binded
+		this.onFormSubmit = this.onFormSubmit.bind(this);
 	}
 
 //event handler
@@ -17,9 +23,14 @@ export default class SearchBar extends Component{
 		this.setState({term: event.target.value});
 	}
 
+//need tp bind context of onFormSubmit
 	onFormSubmit(event){
 		//dont submit the form
 		event.preventDefault();
+		//call to fetch the weather
+		this.props.fetchWeather(this.state.term);
+		//on submit it will clear the search term
+		this.setState({term: ''});
 	}
 
 	render(){
@@ -38,3 +49,14 @@ export default class SearchBar extends Component{
 		);
 	}
 }
+
+//anything return from this function will return as props on the SearchBar container
+function matchDispatchToProps(dispatch){
+	//key of fetchWeather: with a value of fetchWeather we passed in
+	//fetchWeather is set up to flow through the dispatch action to middleware and to the reducers
+	return bindActionCreators({fetchWeather}, dispatch)
+}
+
+//the null takes the place of possible mapStateToProps
+//matchDispatchToProps has to be the 2nd argument
+export default connect(null, matchDispatchToProps)(SearchBar);
